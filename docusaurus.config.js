bc9f82636adfac8f1cedf3ef653e54d5ad2c6d45
andres alcocer <devandres.tech@gmail.com>
2022-08-07 09:16:39 -0400

    setup google analytics

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -41,6 +41,9 @@ const config = {
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
+        gtag: {
+          trackingID: 'G-M827TKJ5P9',
+        },
       }),
     ],
   ],
